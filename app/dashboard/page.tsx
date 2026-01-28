27c396f229ab28a7e50d2636dedabe16aae53277
Carlos Basaez <car.basaez@gmail.com>
2026-01-28 11:36:50 -0300

    fix localstorage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,20 @@
 "use client";
+export const dynamic = "force-dynamic";
 
 import DashboardFooter from "@/components/dashboardFooter";
 import Navbar from "@/components/navbar";
 import ProjectGrid from "@/components/projectGrid";
+import { useEffect, useState } from "react";
 
 
 
 export default function Home() {
+    const [user, setUser] = useState<string | null>(null);
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user_name");
+        setUser(storedUser);
+    }, []);
     return (
         <div>
             <Navbar />
