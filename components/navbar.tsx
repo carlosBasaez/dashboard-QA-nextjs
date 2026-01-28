b20424a99b5c2c164c7bd7a4e025b791bc6b6e42
Carlos Basaez <car.basaez@gmail.com>
2026-01-28 11:52:03 -0300

    fix otro localstorage

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,21 @@
 "use client"
+import { useEffect, useState } from "react";
 import { RootState } from "../src/lib/store"
 import { useSelector } from "react-redux"
 
 
 export default function Navbar() {
-    //para llamar el nombre del redux
+    //llamada a redux
     const name = useSelector((state: RootState) => state.user.name);
-    const namelocal = localStorage.getItem('user_name')
-    //abreviatura para logo
-    const nameAb = namelocal?.slice(0, 2) || "AD";
+
+    const [nameLocal, setNameLocal] = useState<string | null>(null);
+
+    useEffect(() => {
+        const stored = localStorage.getItem("user_name");
+        setNameLocal(stored);
+    }, []);
+
+    const nameAb = nameLocal?.slice(0, 2) || "AD";
 
     return (
         <header className="h-16 border-b border border-[#30363D] bg-[#0B0E11] backdrop-blur-md sticky top-0 px-8 flex items-center justify-between">
@@ -23,7 +30,7 @@ export default function Navbar() {
                     <button className="flex items-center gap-3 pl-1 pr-3 py-1 rounded-full bg-[#161B22] border border-[#30363D] hover:border-slate-500
                     transition-colors">
                         <div>{nameAb}</div>
-                        <span className="text-sm font-medium text-slate-300">{namelocal}</span>
+                        <span className="text-sm font-medium text-slate-300">{nameLocal}</span>
                     </button>
                 </div>
             </div>
